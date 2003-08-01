ba3ea4b52c89b03f9abbafe05964c73f8ad8b190
Shannon Eric Peevey <speeves@erikin.com>
2003-08-01 16:37:02 +0000

    Import of SPEEVES/Apache-AuthzNIS-0.11 from CPAN.
    gitpan-cpan-distribution: Apache-AuthzNIS
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         SPEEVES/Apache-AuthzNIS-0.11.tar.gz
    gitpan-cpan-author:       SPEEVES
    gitpan-cpan-maturity:     released

--- AuthzNIS.pm
+++ AuthzNIS.pm
@@ -1,49 +1,75 @@
 package Apache::AuthzNIS;
 
 use strict;
-use Apache::Constants ':common';
+# use Apache::Constants ':common';
 use Net::NIS;
 
-$Apache::AuthzNIS::VERSION = '0.10';
+$Apache::AuthzNIS::VERSION = '0.11';
+
+############################################
+# here is where we start the new code....
+############################################
+use mod_perl ;
+
+# setting the constants to help identify which version of mod_perl
+# is installed
+use constant MP2 => ($mod_perl::VERSION >= 1.99);
+
+# test for the version of mod_perl, and use the appropriate libraries
+BEGIN {
+        if (MP2) {
+                require Apache::Const;
+                require Apache::Access;
+                require Apache::Connection;
+                require Apache::Log;
+                require Apache::RequestRec;
+                require Apache::RequestUtil;
+                Apache::Const->import(-compile => 'HTTP_UNAUTHORIZED','OK', 'HTTP_INTERNAL_SERVER_ERROR');
+        } else {
+                require Apache::Constants;
+                Apache::Constants->import('HTTP_UNAUTHORIZED','OK', 'HTTP_INTERNAL_SERVER_ERROR');
+        }
+}
+##################### end modperl code ######################
 
 sub handler {
     my $r = shift;
     my $requires = $r->requires;
-    return OK unless $requires;
+    return (MP2 ? Apache::OK : Apache::Constants::OK) unless $requires;
 
-    my $name = $r->connection->user;
+    my $name = MP2 ? $r->user : $r->connection->user;
 
     for my $req (@$requires) {
         my($require, @list) = split /\s+/, $req->{requirement};
 
 	#ok if user is one of these users
 	if ($require eq "user") {
-	    return OK if grep $name eq $_, @list;
+	    return (MP2 ? Apache::OK : Apache::Constants::OK) if grep $name eq $_, @list;
 	}
 	#ok if user is simply authenticated
 	elsif ($require eq "valid-user") {
-	    return OK;
+	    return MP2 ? Apache::OK : Apache::Constants::OK;
 	}
 	elsif ($require eq "group") {
 	    my $domain = Net::NIS::yp_get_default_domain();
 	    unless($domain) {
 		$r->note_basic_auth_failure;
-		$r->log_reason("Apache::AuthenNIS - cannot obtain NIS domain", $r->uri);
-		return SERVER_ERROR;
+		MP2 ? $r->log_error("Apache::AuthenNIS - cannot obtain NIS domain", $r->uri) : $r->log_reason("Apache::AuthenNIS - cannot obtain NIS domain", $r->uri);
+		return MP2 ? Apache::HTTP_INTERNAL_SERVER_ERROR : Apache::Constants::HTTP_INTERNAL_SERVER_ERROR;
 	    }
 	    foreach my $thisgroup (@list) {
 		my ($status, $entry) = Net::NIS::yp_match($domain, "group.byname", $thisgroup);
 		if($status) {
 		    my $error_msg = Net::NIS::yperr_string($status);
 		    $r->note_basic_auth_failure;
-		    $r->log_reason("Apache::AuthzNIS - group: $thisgroup: yp_match status $status, $error_msg", $r->uri);
-		    return SERVER_ERROR;
+		    MP2 ? $r->log_error("Apache::AuthzNIS - group: $thisgroup: yp_match status $status, $error_msg", $r->uri) : $r->log_reason("Apache::AuthzNIS - group: $thisgroup: yp_match status $status, $error_msg", $r->uri);
+			return MP2 ? Apache::HTTP_INTERNAL_SERVER_ERROR : Apache::Constants::HTTP_INTERNAL_SERVER_ERROR;
 		}
 		my @names = split /\,/, $entry;
 		$names[0] =~ s/^.*:.*:.*://;
 		foreach my $oneuser (@names) {
 		    if ($oneuser eq $name) {
-			return OK;
+			    return MP2 ? Apache::OK : Apache::Constants::OK;
 		    }
 		}
 	    }
@@ -51,8 +77,8 @@ sub handler {
     }
     
     $r->note_basic_auth_failure;
-    $r->log_reason("Apache::AuthzNIS - user $name: not authorized", $r->uri);
-    return AUTH_REQUIRED;
+    MP2 ? $r->log_error("Apache::AuthzNIS - user $name: not authorized", $r->uri) : $r->log_reason("Apache::AuthzNIS - user $name: not authorized", $r->uri);
+    return MP2 ? Apache::HTTP_UNAUTHORIZED : Apache::Constants::HTTP_UNAUTHORIZED;
 }
 
 1;
@@ -120,6 +146,7 @@ that it was written hastily, to say the least.
 =head1 AUTHOR
 
 Demetrios E. Paneras <dep@media.mit.edu>
+Ported by Shannon Eric Peevey <speeves@unt.edu>
 
 =head1 COPYRIGHT
 
